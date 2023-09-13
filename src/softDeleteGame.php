243214bad121c0d484658a311b2eaf76e4b937b5
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-09-13 22:54:38 +0100

    cleanup

--- src/softDeleteGame.php
+++ src/softDeleteGame.php
@@ -5,7 +5,8 @@ $id = $_POST['gameId'];
 $query = $db->prepare("
 UPDATE `games` SET `games`.`deleted` = 1 WHERE `games`.`id` = :id;
 ");
+
 $query->bindParam(':id', $id, PDO::PARAM_INT);
-$id = $_POST['gameId'];
 $query->execute();
+
 header("Location: ../index.php");
