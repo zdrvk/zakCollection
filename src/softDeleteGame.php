da442f1e781da6e808a29c87d18f50ac194ba558
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-09-14 16:05:59 +0100

    small tweaks

--- src/softDeleteGame.php
+++ src/softDeleteGame.php
@@ -10,12 +10,11 @@ $deleted = $_POST['deleted'];
 $softDelete = new GameModel($db);
 $success = $softDelete->toggleGameStatus($id, $deleted);
 
-
 if (!$success && $deleted == 0) {
     return header("Location: ../index.php?error=1");
 } else if (!$success && $deleted == 1) {
     return header("Location: ../deletedGames/deletedGames.php?error=1");
-} else if ($deleted === 0) {
+} else if ($deleted == 0) {
     return header("Location: ../index.php");
 } else {
     return header("Location: ../deletedGames/deletedGames.php");
